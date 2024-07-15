7709fb81a7f24b3ba1b58a7bfa1a0bf7bdac7c5f
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-15 21:33:00 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,22 @@
+/* Create a js file which contains variable declaration for a form which asks following basic details :
+ (What is your Name, Email, age, address, phone number, gender,
+ Are you married)*/
+var Name="jaya Patidar";
+var Email="jayapatidar@gmail.com";
+var Age=21;
+var address="indore";
+ var Phone_Number=2345845645;
+ var Gender="Female";
+ var married="No";
+ console.log("Basic Details-:");
+ console.log("Name: "+Name);
+ console.log("Email: "+Email);
+ console.log("Age: "+Age);
+ console.log("Address: "+address);
+ console.log("Phone_Number: "+Phone_Number);
+ console.log("Gender: "+Gender);
+ console.log("Married: "+married);
+
+
+
+
